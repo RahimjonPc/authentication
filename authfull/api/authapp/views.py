b7c973f5926cc4865da9cce080a8c385672dbe64
RahimjonPc <abduganievrahimjon@gmail.com>
2021-01-04 23:47:25 +0500

    smth

---
+++ authfull/api/authapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response 
+from rest_framework import status
+from datetime import datetime
+
+@api_view(['GET'])
+def index(request):
+    date = datetime.now().strftime("%d/%m/%Y %H:%M:/%S")
+    message = 'server is live current time is'
+    return Response(data=message + date, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def restricted(request, *args, **kwargs):
+    return Response(data='Only for Logged in User', status=status.HTTP_200_OK)
